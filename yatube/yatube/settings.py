60ac1c1f130faf33f0bacf3a418acb4541af57ae
Максим Бруцкий <turbosniker@yandex.ru>
2022-05-14 23:12:05 +0300

    удалил в settings LOGOUT

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -130,7 +130,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 LOGIN_URL = 'users:login'
 LOGOUT_URL = 'users:logout'
 LOGIN_REDIRECT_URL = 'posts:index'
-LOGOUT_REDIRECT_URL = 'posts:index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
