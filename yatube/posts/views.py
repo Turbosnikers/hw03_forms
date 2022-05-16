1fe5dfa01ed92f19e6f0742d3bade31d53cef817
Максим Бруцкий <turbosniker@yandex.ru>
2022-05-17 01:07:30 +0300

    исправил замечания после первого ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,98 +1,82 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post, User
-from .models import Group
-from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import PostForm
+from .models import Group, Post, User
 
 POSTS_COUNT = 10
 
 
-def index(request):
-    template = 'posts/index.html'
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, POSTS_COUNT)
+def get_page_context(queryset, request):
+    paginator = Paginator(queryset, POSTS_COUNT)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    context = {
+    return {
+        'paginator': paginator,
+        'page_number': page_number,
         'page_obj': page_obj,
     }
-    return render(request, template, context)
+
+
+def index(request):
+    context = get_page_context(Post.objects.all(), request)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()
-    paginator = Paginator(posts, POSTS_COUNT)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     context = {
-        'page_obj': page_obj,
         'group': group,
     }
-    return render(request, template, context)
+    context.update(get_page_context(group.posts.all(), request))
+    return render(request, 'posts/group_list.html', context)
 
 
 def profile(request, username):
-    template = 'posts/profile.html'
     author = get_object_or_404(User, username=username)
-    author_count = author.posts.count
-    posts = author.posts.all()
-    paginator = Paginator(posts, POSTS_COUNT)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     context = {
         'author': author,
-        'author_count': author_count,
-        'posts': posts,
-        'page_obj': page_obj,
     }
-    return render(request, template, context)
+    context.update(get_page_context(author.posts.all(), request))
+    return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
-    template = 'posts/post_detail.html'
     post = get_object_or_404(Post, id=post_id)
     context = {
         'post': post,
     }
-    return render(request, template, context)
+    return render(request, 'posts/post_detail.html', context)
 
 
 @login_required
 def post_create(request):
-    template = 'posts/create_post.html'
-    title = 'Добавить запись'
-    form = PostForm(request.POST or None)
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.save()
-        return redirect('posts:profile', post.author.username)
-    context = {
-        'title': title,
-        'form': form,
-    }
-    return render(request, template, context)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:profile', post.author.username)
+    form = PostForm(request.POST)
+    return render(request, 'posts/create_post.html', {'form': form})
 
 
 @login_required
 def post_edit(request, post_id):
-    template = 'posts/create_post.html'
-    title = 'Редактировать запись'
     post = get_object_or_404(Post, id=post_id)
     if post.author != request.user:
         return redirect('posts:post_detail', post_id=post_id)
-    form = PostForm(request.POST or None, instance=post)
-    if form.is_valid():
-        form = form.save()
-        return redirect('posts:post_detail', post_id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form = form.save()
+            return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm(instance=post)
     context = {
-        'title': title,
         'form': form,
         'is_edit': True,
         'post_id': post.id,
     }
-    return render(request, template, context)
+    return render(request, 'posts/create_post.html', context)
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,12 +1,8 @@
-from django.contrib.auth.views import (LogoutView,
-                                       LoginView,
-                                       PasswordChangeView,
-                                       PasswordChangeDoneView,
-                                       PasswordResetView,
+from django.contrib.auth.views import (LoginView, LogoutView,
                                        PasswordResetDoneView,
-                                       PasswordResetConfirmView,
-                                       PasswordResetCompleteView)
+                                       PasswordResetView)
 from django.urls import path
+
 from . import views
 
 app_name = 'users'
@@ -24,18 +20,6 @@ urlpatterns = [
         name='login'
     ),
     path(
-        'password_change/',
-        PasswordChangeView.as_view(
-            template_name='users/password_change_form.html'),
-        name='password_change'
-    ),
-    path(
-        'password_change/done/',
-        PasswordChangeDoneView.as_view(
-            template_name='users/password_change_done.html'),
-        name='password_change_done'
-    ),
-    path(
         'password_reset/',
         PasswordResetView.as_view(
             template_name='users/password_reset_form.html'),
@@ -46,17 +30,5 @@ urlpatterns = [
         PasswordResetDoneView.as_view(
             template_name='users/password_reset_done.html'),
         name='password_reset_done'
-    ),
-    path(
-        'reset/<uidb64>/<token>',
-        PasswordResetConfirmView.as_view(
-            template_name='users/password_reset_confirm.html'),
-        name='password_reset_confirm'
-    ),
-    path(
-        'reset/done',
-        PasswordResetCompleteView.as_view(
-            template_name='users/password_reset_complete.html'),
-        name='password_reset_complete'
     ),
 ]
